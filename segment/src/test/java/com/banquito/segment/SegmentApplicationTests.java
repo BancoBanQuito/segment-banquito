5418c5d388ac8695cce3ebb17f6c7bb3b453a9f1
Abigail Cabascango <51092317+abigailscl@users.noreply.github.com>
2023-01-08 22:32:41 -0500

    Add files via upload

---
+++ segment/src/test/java/com/banquito/segment/SegmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banquito.segment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
